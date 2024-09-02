699723188556032c1f7ed6a50be1cab426749951
Tran Ngoc Phuong Linh <tranngocphuonglinh308@gmail.com>
2024-09-02 14:02:38 +0700

    Updated

---
+++ music/music.js
@@ -0,0 +1,88 @@
+// const audio = document.querySelector("audio");
+
+// // Example: Add custom play/pause buttons
+// const playButton = document.getElementById("play-button");
+// const pauseButton = document.getElementById("pause-button");
+
+// playButton.addEventListener("click", () => {
+//     audio.play();
+// });
+
+// pauseButton.addEventListener("click", () => {
+//     audio.pause();
+// });
+
+const musicContainer = document.getElementById("music-container");
+const playBtn = document.getElementById("play");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+
+const audio = document.getElementById("audio");
+const progress = document.getElementById("progress");
+const progressContainer = document.getElementById("progress-container");
+const title = document.getElementById("title");
+const cover = document.getElementById("cover");
+let curr_time = document.querySelector(".current-time");
+let total_duration = document.querySelector(".total-duration");
+
+// Play song
+function playSong() {
+    musicContainer.classList.add("play");
+    playBtn.querySelector("i.fas").classList.remove("fa-play");
+    playBtn.querySelector("i.fas").classList.add("fa-pause");
+
+    audio.play();
+}
+
+// Pause song
+function pauseSong() {
+    musicContainer.classList.remove("play");
+    playBtn.querySelector("i.fas").classList.add("fa-play");
+    playBtn.querySelector("i.fas").classList.remove("fa-pause");
+
+    audio.pause();
+}
+
+// Update progress bar
+function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+}
+
+// Set progress bar
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
+}
+
+// Lắng nghe sự kiện
+playBtn.addEventListener("click", () => {
+    // Kiểm tra xem musicContainer có chứa class "play" hay không?
+    const isPlaying = musicContainer.classList.contains("play");
+
+    // Nếu có thì thực hiện pause
+    // Nếu không thì thực hiện play
+    if (isPlaying) {
+        pauseSong();
+    } else {
+        playSong();
+    }
+});
+
+// Time/song update
+audio.addEventListener("timeupdate", updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener("click", setProgress);
+
+// Reset progress bar when song ends
+audio.addEventListener("ended", () => {
+    progress.style.width = "0%";
+    musicContainer.classList.remove("play");
+    playBtn.querySelector("i.fas").classList.add("fa-play");
+    playBtn.querySelector("i.fas").classList.remove("fa-pause");
+});
